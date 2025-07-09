d6bf8d7d404b454e5c2a77ca0e63a601804284d8
jaluspeet <39632922+jaluspeet@users.noreply.github.com>
2025-07-09 22:21:08 +0200

    Update resnet.py

--- nets/resnet.py
+++ nets/resnet.py
@@ -6,7 +6,6 @@ class ResNet(nn.Module):
         super().__init__()
         base_model = models.resnet18(weights=None)
         base_model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        # (no need to adjust weights if using a randomly initialized model)
         in_feats = base_model.fc.in_features
         base_model.fc = nn.Linear(in_feats, num_classes)
         self.model = base_model
